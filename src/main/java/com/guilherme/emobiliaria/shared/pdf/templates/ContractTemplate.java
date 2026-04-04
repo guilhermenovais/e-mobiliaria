fa4e64165d0d6a004aea5faa6cb078d3134bcd39
guilherme <guilhermenovaisdesouza@proton.me>
2026-04-04 13:06:37 -0300

    chore: update contract pdf structure

--- src/main/java/com/guilherme/emobiliaria/shared/pdf/templates/ContractTemplate.java
+++ src/main/java/com/guilherme/emobiliaria/shared/pdf/templates/ContractTemplate.java
@@ -1,95 +1,71 @@
 package com.guilherme.emobiliaria.shared.pdf.templates;
 
 import com.guilherme.emobiliaria.contract.domain.entity.Contract;
-import com.guilherme.emobiliaria.contract.domain.entity.PaymentAccount;
-import com.guilherme.emobiliaria.person.domain.entity.Person;
-import com.guilherme.emobiliaria.property.domain.entity.Property;
-import com.guilherme.emobiliaria.property.domain.entity.Purpose;
 import com.guilherme.emobiliaria.shared.pdf.PdfTemplate;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.EnumMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class ContractTemplate extends PdfTemplate<ContractTemplate.ContractParameters, ContractTemplate.ContractCollections> {
+public class ContractTemplate
+    extends PdfTemplate<ContractTemplate.ContractParameters, ContractTemplate.ContractCollections> {
 
-  public enum ContractParameters {
-    LANDLORD_NAME, LANDLORD_DESCRIPTION, PROPERTY_TYPE, PROPERTY_ADDRESS,
-    PROPERTY_PURPOSE, PROPERTY_RENT_VALUE, PROPERTY_CEMIG, PROPERTY_IPTU,
-    CONTRACT_PERIOD, CONTRACT_START_DATE, CONTRACT_END_DATE, CONTRACT_PAYMENT_METHOD,
-    LANDLORD_CITY, CONTRACT_START_DATE_IN_FULL, LANDLORD_SIGNING, TENANT_SIGNING
-  }
-
-  public enum ContractCollections {
-    TENANTS_LIST
-  }
-
+  @SuppressWarnings("unused")
   private final Contract contract;
+
+  @Override
+  public EnumMap<ContractParameters, Object> getParameters() {
+    EnumMap<ContractParameters, Object> params = new EnumMap<>(ContractParameters.class);
+    params.put(ContractParameters.CONTRACT_TITLE, "CONTRATO DE LOCAÇÃO");
+    params.put(ContractParameters.LANDLORD_TEXT,
+        "LOCADORA: Maria Souza, brasileira, casada, empresaria, CPF 111.222.333-44.");
+    params.put(ContractParameters.TENANTS_TEXT,
+        "LOCATARIOS: Joao Silva e Ana Lima, brasileiros, maiores e capazes.");
+    params.put(ContractParameters.PROPERTY_SECTION_TITLE, "1) DO IMOVEL");
+    params.put(ContractParameters.PROPERTY_TYPE_TEXT, "Tipo: Apartamento");
+    params.put(ContractParameters.PROPERTY_ADDRESS_TEXT,
+        "Endereco: Rua das Flores, 100, Centro, Belo Horizonte - MG");
+    params.put(ContractParameters.PROPERTY_PURPOSE_TEXT, "Finalidade: Residencial");
+    params.put(ContractParameters.CONTRACT_RENT_TEXT, "Aluguel mensal: R$ 1.500,00");
+    params.put(ContractParameters.PROPERTY_CEMIG_TEXT, "CEMIG: 123456789");
+    params.put(ContractParameters.PROPERTY_IPTU_TEXT, "IPTU: 2026-000123");
+    params.put(ContractParameters.PERIOD_SECTION_TITLE, "2) DO PRAZO E PAGAMENTO");
+    params.put(ContractParameters.CONTRACT_PERIOD_TEXT, "Prazo: 12 meses");
+    params.put(ContractParameters.CONTRACT_START_DATE_TEXT, "Inicio: 01/01/2026");
+    params.put(ContractParameters.CONTRACT_END_DATE_TEXT, "Termino: 01/01/2027");
+    params.put(ContractParameters.CONTRACT_PAYMENT_METHOD_TEXT,
+        "Pagamento via Banco do Brasil, Agencia 1234-5, Conta 12345-6, PIX: pix@exemplo.com");
+    params.put(ContractParameters.CONTRACTUAL_TERMS_SECTION_TITLE, "3) DAS CONDICOES GERAIS");
+    params.put(ContractParameters.CONTRACTUAL_TERMS_TEXT,
+        "O(a) LOCATARIO(a) se compromete a manter o imovel em bom estado de conservacao e uso.");
+    params.put(ContractParameters.CITY_AND_DATE_TEXT, "Belo Horizonte, 1 de janeiro de 2026.");
+    return params;
+  }
+
+  @Override
+  public EnumMap<ContractCollections, Collection<Object>> getCollections() {
+    EnumMap<ContractCollections, Collection<Object>> collections =
+        new EnumMap<>(ContractCollections.class);
+    collections.put(ContractCollections.SIGNING_TEXTS,
+        List.of(new TextBean("Maria Souza - LOCADORA"),
+            new TextBean("Joao Silva e Ana Lima - LOCATARIOS")));
+    return collections;
+  }
 
   public ContractTemplate(Contract contract) {
     super("contract");
     this.contract = contract;
   }
 
-  @Override
-  public EnumMap<ContractParameters, Object> getParameters() {
-    EnumMap<ContractParameters, Object> params = new EnumMap<>(ContractParameters.class);
-    Person landlord = contract.getLandlord();
-    Property property = contract.getProperty();
-    PaymentAccount account = contract.getPaymentAccount();
-    LocalDate startDate = contract.getStartDate();
-    LocalDate endDate = startDate.plus(contract.getDuration());
-
-    params.put(ContractParameters.LANDLORD_NAME, TemplateFormatter.personName(landlord));
-    params.put(ContractParameters.LANDLORD_DESCRIPTION, TemplateFormatter.personDescription(landlord));
-    params.put(ContractParameters.PROPERTY_TYPE, property.getType());
-    params.put(ContractParameters.PROPERTY_ADDRESS, TemplateFormatter.formatAddress(property.getAddress()));
-    params.put(ContractParameters.PROPERTY_PURPOSE, purposeInPortuguese(property.getPurpose()));
-    params.put(ContractParameters.PROPERTY_RENT_VALUE, TemplateFormatter.formatCurrency(contract.getRent()));
-    params.put(ContractParameters.PROPERTY_CEMIG, property.getCemig());
-    params.put(ContractParameters.PROPERTY_IPTU, property.getIptu());
-    params.put(ContractParameters.CONTRACT_PERIOD, TemplateFormatter.formatPeriod(contract.getDuration()));
-    params.put(ContractParameters.CONTRACT_START_DATE, TemplateFormatter.formatDate(startDate));
-    params.put(ContractParameters.CONTRACT_END_DATE, TemplateFormatter.formatDate(endDate));
-    params.put(ContractParameters.CONTRACT_PAYMENT_METHOD, formatPaymentMethod(account));
-    params.put(ContractParameters.LANDLORD_CITY, TemplateFormatter.personCity(landlord));
-    params.put(ContractParameters.CONTRACT_START_DATE_IN_FULL, TemplateFormatter.formatDateInFull(startDate));
-    params.put(ContractParameters.LANDLORD_SIGNING, TemplateFormatter.personName(landlord));
-    params.put(ContractParameters.TENANT_SIGNING, contract.getTenants().stream()
-        .map(TemplateFormatter::personName)
-        .collect(Collectors.joining(", ")));
-    return params;
+  public enum ContractParameters {
+    CONTRACT_TITLE, LANDLORD_TEXT, TENANTS_TEXT, PROPERTY_SECTION_TITLE, PROPERTY_TYPE_TEXT, PROPERTY_ADDRESS_TEXT, PROPERTY_PURPOSE_TEXT, CONTRACT_RENT_TEXT, PROPERTY_CEMIG_TEXT, PROPERTY_IPTU_TEXT, PERIOD_SECTION_TITLE, CONTRACT_PERIOD_TEXT, CONTRACT_START_DATE_TEXT, CONTRACT_END_DATE_TEXT, CONTRACT_PAYMENT_METHOD_TEXT, CONTRACTUAL_TERMS_SECTION_TITLE, CONTRACTUAL_TERMS_TEXT, CITY_AND_DATE_TEXT
   }
 
-  @Override
-  public EnumMap<ContractCollections, Collection<Object>> getCollections() {
-    EnumMap<ContractCollections, Collection<Object>> collections = new EnumMap<>(ContractCollections.class);
-    List<Object> tenantBeans = contract.getTenants().stream()
-        .map(t -> (Object) new TenantBean(
-            TemplateFormatter.personName(t),
-            TemplateFormatter.personDescription(t)))
-        .collect(Collectors.toList());
-    collections.put(ContractCollections.TENANTS_LIST, tenantBeans);
-    return collections;
+  public enum ContractCollections {
+    SIGNING_TEXTS
   }
 
-  private String formatPaymentMethod(PaymentAccount account) {
-    StringBuilder sb = new StringBuilder();
-    sb.append(account.getBank())
-        .append(", Agência ").append(account.getBankBranch())
-        .append(", Conta ").append(account.getAccountNumber());
-    if (account.getPixKey() != null && !account.getPixKey().isBlank()) {
-      sb.append(", PIX: ").append(account.getPixKey());
-    }
-    return sb.toString();
-  }
 
-  private String purposeInPortuguese(Purpose purpose) {
-    return switch (purpose) {
-      case RESIDENTIAL -> "Residencial";
-      case COMMERCIAL -> "Comercial";
-    };
+  public record TextBean(String text) {
   }
 }
